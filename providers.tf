e4bd0790f12f8dd1c1ad11eaea7a15d4e5958f5c
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-07-28 13:21:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  aws_access_key_id  = "$AWSaws_access_key_id"
-  aws_secret_access_key = "$AWS_Secret_access_key" 
+  aws_access_key_id  = ""
+  aws_secret_access_key = "" 
 }
