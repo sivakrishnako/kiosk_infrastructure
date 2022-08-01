70a1a83d65464fd1783349340a33514fc6fb6266
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-08-01 12:32:58 +0530

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -3,7 +3,7 @@ resource "aws_sqs_queue" "this" {
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 11
   tags = {
     Name                    = "${lower(local.local_data.tag_prefix)}-dailybatchdata-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
   }
