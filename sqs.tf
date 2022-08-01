196bac679fb14555ac9f2e67bab8c73e6f4d6864
Sushant Sonarghare <sushants@cybage.com>
2022-08-01 17:16:58 +0530

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "this" {
-  name                      = "${lower(local.local_data.tag_prefix)}-dailybatchdata-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+  name                      = ${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
   tags = {
-    Name                    = "${lower(local.local_data.tag_prefix)}-dailybatchdata-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+    Name                    = ${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}
   }
 }
