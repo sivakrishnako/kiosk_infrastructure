09e57f27659ed4d9bb34599d3532d80318d117ab
Sushant Sonarghare <sushants@cybage.com>
2022-08-01 17:19:33 +0530

    Update lamda.tf

--- lamda.tf
+++ lamda.tf
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "kiosk-lambda" {
   s3_bucket = "kiosk-rtree-api-new" 
   s3_key = "AspNetCoreFunction-CodeUri-Or-ImageUri-637890741703279843-637890743207211420 (1).zip"
 #   s3_object_version = local.s3_object_version
-  function_name = "rt-s3-eventbridge-sqs-dailybatch-process-sqa-kiosk"
+  function_name = ${lower(local.local_data.tag_prefix)}-s3-eventbridge-sqs-dailybatch-process-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
 
